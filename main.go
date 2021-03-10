b35c279e00eb60d8ad277b49c00ccfab802d5fde
christophervenegas2 <christophervenegas2@gmail.com>
2021-03-09 21:21:42 -0300

    Se agrega el DELETE y algunos comentarios en los Headers

--- main.go
+++ main.go
@@ -44,6 +44,7 @@ func createCreditCard(c *gin.Context) {
 
 	c.JSON(200, gin.H{
 		"message": "Tarjeta creada con exito",
+		"tarjeta": CreditCards,
 	})
 
 }
@@ -65,6 +66,7 @@ func updateCreditCard(c *gin.Context) {
 
 			c.JSON(200, gin.H{
 				"message": "Se realizaron los cambios correctamente",
+				"tarjeta": CreditCards[i],
 			})
 			return
 		}
@@ -74,7 +76,32 @@ func updateCreditCard(c *gin.Context) {
 	})
 }
 
-func deleteCreditCard(c *gin.Context) {}
+func deleteCreditCard(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, u := range CreditCards {
+		if u.ID == id {
+
+			// * Trucazo de un video
+			// * t := [1,2,3,4,5,6,7]
+			// * t[:2] == [1,2]
+			// ! t[2:] == [3,4,5,6,7]
+			// * t[2 + 1:] == [4,5,6,7] ([3:])
+			// ? 	[1,2]	[4,5,6,7] =	[1,2,4,5,6,7] 😁
+
+			CreditCards = append(CreditCards[:i], CreditCards[i+1:]...)
+
+			c.JSON(200, gin.H{
+				"message":  "Se a eliminado con exito la targeta",
+				"targetas": CreditCards,
+			})
+			return
+		}
+		c.JSON(404, gin.H{
+			"message": "No se pudo encontrar a la targeta de credito señalada",
+		})
+	}
+}
 
 func main() {
 	CreditCards = append(CreditCards, CreditCard{
